04a00a011a14f69d15f6148975a4c65ae5a18f67
JonardRosas <jonardrosas@yahoo.com>
2016-10-24 15:55:46 +0000

    REturnetes/base.htmld back the files commited by gentle after the revertion of the admin page 	modifietes/base.htmld:   assets/css/custom.css 	modified:   journal_app/admin.py 	deleted:    journal_app/forms.pyc 	modified:   journal_app/models.py 	modified:   journal_app/templates/home.html 	modified:   templates/base.html

--- journal_app/admin.py
+++ journal_app/admin.py
@@ -5,12 +5,25 @@ from django.contrib import admin
 
 from models import Journal, Journal_entry
 
+admin.site.site_header = 'W.E.D Administration'
 
 class JournalAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
+    list_display = ['__unicode__','created_by_field', 'date_created_field']
 
 class JournalEntryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
+    list_display = ['__unicode__','entry_date_created','get_journal','get_user']
+
+    def get_user(self, obj):
+        return obj.journal_id.created_by
+    get_user.short_description = 'Created by'
+    get_user.admin_order_field = 'journal_id__created_by'
+
+    def get_journal(self, obj):
+        return obj.journal_id.name
+    get_journal.short_description = 'Journal'
+    get_journal.admin_order_field = 'journal_id__name'
 
 admin.site.register(Journal, JournalAdmin)
 admin.site.register(Journal_entry, JournalEntryAdmin)
