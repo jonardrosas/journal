be1bea022b0ec377259a24500d15ea3605943dc6
gentlejoseph <writetogentle@gmail.com>
2016-10-24 10:13:35 +0000

    (1)Styling (2)Models conflict fix (3)Bug fixes 	modified:   assets/css/custom.css 	modified:   journal_app/admin.py 	modified:   journal_app/models.py 	modified:   journal_app/templates/home.html 	modified:   templates/base.html

--- journal_app/models.py
+++ journal_app/models.py
@@ -7,12 +7,21 @@ from django.contrib.auth.models import User
 class Journal(models.Model):
     name = models.CharField(max_length=120)
     description = models.CharField(max_length=500)
-    created_by = models.ForeignKey(User, related_name='+')
+    created_by = models.ForeignKey(User, related_name='created_by')
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __unicode__(self):
         return (self.name)
+
+    def created_by_field(self):
+       return self.created_by
+    created_by_field.short_description = 'Created by'
+
+    def date_created_field(self):
+       return self.date_created
+    date_created_field.short_description = 'Date created'
+
 
 
 
@@ -20,8 +29,8 @@ class Journal_entry(models.Model):
     journal_id = models.ForeignKey(
         Journal,
         related_name="journal_entry",
-        null=True,
-        blank=True, default=None
+        null=False,
+        blank=False, default=None
     )
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
@@ -30,3 +39,7 @@ class Journal_entry(models.Model):
 
     def __unicode__(self):
         return (self.title)
+
+    def entry_date_created(self):
+       return self.date_created
+    entry_date_created.short_description = 'Date created'
