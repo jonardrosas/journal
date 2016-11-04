996ff6b30d8ffa4b1ce89f27d20ec5dc2debeafe
jonardrosas <jonard.rosas@globalfoundries.com>
2016-11-04 17:28:26 +0800

    Added enhancement 	modified:   accounts/admin.py 	modified:   accounts/views.py 	modified:   journal_app/templates/home.html 	modified:   journal_app/views.py 	modified:   templates/base.html

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
-from .models import UserProfile
+from django import forms
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import UserProfile
+from django.contrib.auth.models import User, Group
 
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'text_password']
+class UserProfileAdmin(UserAdmin):
+    list_display = ['first_name', 'last_name', 'username', 'email', 'password', 'get_text_password']
+    #exclude = ['is_staff', 'is_active', 'date_joined', 'last_login', 'is_superuser', 'user_permissions', 'groups']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2'),
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        text_password = form.cleaned_data['password1']
+        if hasattr(obj, 'profile'):
+            obj.profile.text_password = password
+            obj.save()
+        else:
+            UserProfile.objects.create(
+                user=obj,
+                text_password=text_password
+            )
+
+    def get_text_password(self, obj):
+        return obj.profile.text_password
+    get_text_password.short_description = 'Text Password'
+    get_text_password.admin_order_field = 'profile__text_password'
 
 # Register your models here.
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(User, UserProfileAdmin)
